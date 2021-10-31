c4fd8d248eee7275645abe04877c2a923b29b54b
gvillagra <gvillagraa@hotmail.com>
2021-10-31 20:55:17 -0300

    installed express-validator

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body, check, validationResult } = require('express-validator');
 
 const usersController = require('../controllers/usersController');
 
